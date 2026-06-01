e5fd17c6dad12a03d5d3daaeb1da33d0f440f654
Dave Barry <david.barry@crick.ac.uk>
2026-06-01 15:07:22 +0100

    Added submit_bigstitcher_jobs.sh

--- run_bigstitcher.sh
+++ run_bigstitcher.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=brainsaw-define-dataset
+#SBATCH --job-name=brainsaw-stitch
 #SBATCH --ntasks=1
 #SBATCH --time=1-00:00:00
 #SBATCH --mem=256G
@@ -7,11 +7,8 @@
 
 ml Java/1.8
 
-#INPUT_DIR="/nemo/stp/lm/working/barryd/hpc/projects/labs/miguel-aliaga/elisa/brainsaw-tiff-converter-outputs/CrickSaw_260326_hml_old_young_females_hml-0003"
-OUTPUT_DIR="/nemo/stp/lm/working/barryd/hpc/projects/labs/miguel-aliaga/elisa/brainsaw-bigstitcher-tiff-converter-outputs/CrickSaw_260326_hml_old_young_females_hml-0001-0003-fused"
-INPUT_DIR="/nemo/stp/lm/working/barryd/hpc/projects/labs/miguel-aliaga/elisa/brainsaw-bigstitcher-tiff-converter-outputs"
-
-
 mkdir -p "$OUTPUT_DIR"
 
-/nemo/stp/lm/working/barryd/hpc/Fiji.app/ImageJ-linux64 --headless -macro /nemo/stp/lm/working/barryd/hpc/projects/labs/miguel-aliaga/elisa/scripts/define_dataset.ijm "$INPUT_DIR,$OUTPUT_DIR"
+/nemo/stp/lm/working/barryd/hpc/Fiji.app/ImageJ-linux64 --headless -macro \
+    /nemo/stp/lm/working/barryd/hpc/projects/labs/miguel-aliaga/elisa/scripts/define_dataset.ijm \
+    "$INPUT_DIR,$OUTPUT_DIR"
