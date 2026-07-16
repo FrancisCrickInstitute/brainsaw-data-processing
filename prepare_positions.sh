9c278665c9f7755a65daeb10aa71a99429632cde
Dave Barry <david.barry@crick.ac.uk>
2026-07-16 15:26:00 +0100

    Add `prepare_positions.sh` script for job submission with Pixi

---
+++ prepare_positions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --job-name=brainsaw-positions
+#SBATCH --ntasks=1
+#SBATCH --mem=1G
+#SBATCH --partition=ncpu
+# Note: --export is set dynamically by the submission script
+
+ml pixi
+
+pixi run python ./mat_to_csv.py "$INPUT_DIR"
